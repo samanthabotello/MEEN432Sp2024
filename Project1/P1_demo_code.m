4c3249a772cf11277ccb39941f6758124253297c
JeremiahJen <157443280+JeremiahJen@users.noreply.github.com>
2024-02-02 17:15:20 -0600

    Update P1_demo_code.m
    Graph of ODE4 with different time steps

--- Project1/P1_demo_code.m
+++ Project1/P1_demo_code.m
@@ -3,15 +3,23 @@ w_0_values = [0,10]; % Initial Angular Velocity [rad/s]
 J_values = [.01,100]; % Rotational Inertia [kg-m^2]
 b_values = [.1,10]; % Damping Coefficient [N-m-s/rad]
 A_values = [0,100]; % Constant Applied Torque [N-m]
+time_step = [.001,.1,1];
 w_0 = 0;
 A = 0;
 J = 0;
 b = 0;
 set_param('Project1', 'StopTime', '25')
 cpu = [];
-cpu = fixedstep("ode1",".001",w_0_values,J_values,b_values,A_values,cpu);
-cpu
+cpu1 = fixedstep("ode4",".001",w_0_values,J_values,b_values,A_values,cpu);
+cpu2 = fixedstep("ode4",".1",w_0_values,J_values,b_values,A_values,cpu);
+cpu3 = fixedstep("ode4","1",w_0_values,J_values,b_values,A_values,cpu);
+cpu = [cpu1',cpu2',cpu3']
+plot(time_step,cpu)
+legend
+xlabel("Step Size")
+ylabel("Cpu Time")
 %Plotting ODE 1 with each stepsize.
+
 
 function [cpu] = fixedstep(solver,stepsize,w_0_values,J_values,b_values,A_values,cpu)
 
