a2e7c2617835fd0dcc1c87327dbcfd2ca2212cad
samanthabotello <73797271+samanthabotello@users.noreply.github.com>
2024-02-04 19:02:23 -0600

    Update P1_demo_code.m
    added ode 4 and variable time

--- Project1/P1_demo_code.m
+++ Project1/P1_demo_code.m
@@ -1,3 +1,4 @@
+
 % Initial Conditions 
 w_0_values = [0,10]; % Initial Angular Velocity [rad/s]
 J_values = [.01,100]; % Rotational Inertia [kg-m^2]
@@ -9,18 +10,38 @@ A = 0;
 J = 0;
 b = 0;
 set_param('Project1', 'StopTime', '25')
+
+%Plotting ODE with each stepsize.
+% ode 4
 cpu = [];
-cpu1 = fixedstep("ode4",".001",w_0_values,J_values,b_values,A_values,cpu);
-cpu2 = fixedstep("ode4",".1",w_0_values,J_values,b_values,A_values,cpu);
-cpu3 = fixedstep("ode4","1",w_0_values,J_values,b_values,A_values,cpu);
-cpu = [cpu1',cpu2',cpu3']
+cpu_ode4_001 = fixedstep("ode4",".001",w_0_values,J_values,b_values,A_values,cpu);
+cpu2_ode4_01 = fixedstep("ode4",".1",w_0_values,J_values,b_values,A_values,cpu);
+cpu3_ode4_1 = fixedstep("ode4","1",w_0_values,J_values,b_values,A_values,cpu);
+cpu = [cpu_ode4_001',cpu2_ode4_01',cpu3_ode4_1']
+figure(1)
 plot(time_step,cpu)
 legend
 xlabel("Step Size")
 ylabel("Cpu Time")
-%Plotting ODE 1 with each stepsize.
+title("Cpu vs Step Size Ode 4")
 
+% ode 1
+cpu = [];
+cpu_ode1_001 = fixedstep("ode1",".001",w_0_values,J_values,b_values,A_values,cpu);
+cpu2_ode1_01 = fixedstep("ode1",".1",w_0_values,J_values,b_values,A_values,cpu);
+cpu3_ode1_1 = fixedstep("ode1","1",w_0_values,J_values,b_values,A_values,cpu);
+cpu = [cpu_ode1_001',cpu2_ode1_01',cpu3_ode1_1']
+figure(2)
+plot(time_step,cpu)
+legend
+xlabel("Step Size")
+ylabel("Cpu Time")
+title("Cpu vs Step Size Ode 1")
 
+% variable time step
+var_time_step_solver = ["ode45", "ode23tb"]; 
+
+% function for fixed step
 function [cpu] = fixedstep(solver,stepsize,w_0_values,J_values,b_values,A_values,cpu)
 
     for i = 1:length(w_0_values)
@@ -34,6 +55,29 @@ function [cpu] = fixedstep(solver,stepsize,w_0_values,J_values,b_values,A_values
                   
                     tStart = cputime;
                     simout = sim("Project1.slx","Solver",solver,"FixedStep",stepsize);
+                    tTotal = cputime - tStart;
+                    cpu(end+1) = tTotal;
+                end
+            end
+        end
+    end
+   
+end
+
+% function for variable time
+function [cpu] = variabletime(solver,w_0_values,J_values,b_values,A_values,cpu)
+
+    for i = 1:length(w_0_values)
+        w_0 = w_0_values(i);
+        for j = 1:length(J_values)
+            J = J_values(j);
+            for k = 1:length(b_values)
+                b = b_values(k);
+                for l = 1:length(A_values)
+                    A = A_values(l);
+                  
+                    tStart = cputime;
+                    simout = sim("Project1.slx","Solver",solver)
                     tTotal = cputime - tStart;
                     cpu(end+1) = tTotal;
                 end
