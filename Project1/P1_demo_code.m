18f1b86f7249fefbd86cd5ff9c4cbb8baa4377f5
JeremiahJen <157443280+JeremiahJen@users.noreply.github.com>
2024-02-09 14:22:02 -0600

    Update P1_demo_code.m
    Code should run but the error calculation has known problems

--- Project1/P1_demo_code.m
+++ Project1/P1_demo_code.m
@@ -12,9 +12,9 @@ set_param('Project1', 'StopTime', '25')
 
 % Plotting ODE with each stepsize.
 % ode 4
-cpu_ode4_001,error_ode4_001 = fixedstep("ode4",".001",w_0_values,J_values,b_values,A_values);
-cpu2_ode4_01,error_ode4_01 = fixedstep("ode4",".1",w_0_values,J_values,b_values,A_values);
-cpu3_ode4_1,error_ode4_1 = fixedstep("ode4","1",w_0_values,J_values,b_values,A_values);
+[cpu_ode4_001,error_ode4_001] = fixedstep("ode4",".001",w_0_values,J_values,b_values,A_values);
+[cpu2_ode4_01,error_ode4_01] = fixedstep("ode4",".1",w_0_values,J_values,b_values,A_values);
+[cpu3_ode4_1,error_ode4_1] = fixedstep("ode4","1",w_0_values,J_values,b_values,A_values);
 
 figure(1)
 cpu = [cpu_ode4_001',cpu2_ode4_01',cpu3_ode4_1'];
@@ -38,9 +38,9 @@ ylabel("Max Error")
 title("Error vs CPU time Ode 4")
 
 % ode 1
-cpu_ode1_001,error_ode1_001 = fixedstep("ode1",".001",w_0_values,J_values,b_values,A_values);
-cpu2_ode1_01,error_ode1_01 = fixedstep("ode1",".1",w_0_values,J_values,b_values,A_values);
-cpu3_ode1_1,error_ode1_1 = fixedstep("ode1","1",w_0_values,J_values,b_values,A_values);
+[cpu_ode1_001,error_ode1_001] = fixedstep("ode1",".001",w_0_values,J_values,b_values,A_values);
+[cpu2_ode1_01,error_ode1_01] = fixedstep("ode1",".1",w_0_values,J_values,b_values,A_values);
+[cpu3_ode1_1,error_ode1_1] = fixedstep("ode1","1",w_0_values,J_values,b_values,A_values);
 
 figure(4)
 cpu = [cpu_ode1_001',cpu2_ode1_01',cpu3_ode1_1'];
@@ -65,7 +65,7 @@ title("Error vs CPU time Ode 1")
 
 
 % ode 45
-cpu,error = variabletime("ode45",w_0_values,J_values,b_values,A_values);
+[cpu,error] = variabletime("ode45",w_0_values,J_values,b_values,A_values);
 
 figure(7)
 plot(cpu,error)
@@ -75,7 +75,7 @@ title("Error vs CPU time Ode45")
 
 
 % ode 23tb
-cpu,error = variabletime("ode23tb",w_0_values,J_values,b_values,A_values);
+[cpu,error] = variabletime("ode23tb",w_0_values,J_values,b_values,A_values);
 
 figure(8)
 plot(cpu,error)
@@ -86,6 +86,7 @@ title("Error vs CPU time Ode23tb")
 % function for fixed step
 function [cpu,error] = fixedstep(solver,stepsize,w_0_values,J_values,b_values,A_values)
     cpu = [];
+    error = [];
     for i = 1:length(w_0_values)
         w_0 = w_0_values(i);
         for j = 1:length(J_values)
@@ -94,7 +95,8 @@ function [cpu,error] = fixedstep(solver,stepsize,w_0_values,J_values,b_values,A_
                 b = b_values(k);
                 for l = 1:length(A_values)
                     A = A_values(l);
-                  
+                    
+                    %CPU time calculation
                     tStart = cputime;
                     simout = sim("Project1.slx","Solver",solver,"FixedStep",stepsize);
                     tTotal = cputime - tStart;
@@ -109,24 +111,23 @@ function [cpu,error] = fixedstep(solver,stepsize,w_0_values,J_values,b_values,A_
                     W = simout.w.Data;
                     max_error = 0;
                     for q = 1:length(W)
-                        error = (abs(true_val(q)-W(q)));
-                        if (error> max_error)
-                            max_error = error;
+                        error_temp = abs(true_val(q)-W(q));
+                        if (error_temp > max_error)
+                            max_error = error_temp;
                         end
                     end
-                    
                     error(end+1) = max_error;
-
+                  
                 end
             end
         end
     end
-   
 end
 
 % function for variable time
-function [cpu] = variabletime(solver,w_0_values,J_values,b_values,A_values)
+function [cpu,error] = variabletime(solver,w_0_values,J_values,b_values,A_values)
     cpu = [];
+    error = [];
     for i = 1:length(w_0_values)
         w_0 = w_0_values(i);
         for j = 1:length(J_values)
@@ -140,7 +141,22 @@ function [cpu] = variabletime(solver,w_0_values,J_values,b_values,A_values)
                     simout = sim("Project1.slx","Solver",solver);
                     tTotal = cputime - tStart;
                     cpu(end+1) = tTotal;
-
+                    
+                    %Error Calculation
+                    T = simout.tout;
+                    true_val = [];
+                    for t = 1:length(T)
+                        true_val(end+1) = model(b,J,A,T(t),w_0);
+                    end
+                    W = simout.w.Data;
+                    max_error = 0;
+                    for q = 1:length(W)
+                        error_temp = abs(true_val(q)-W(q));
+                        if (error_temp > max_error)
+                            max_error = error_temp;
+                        end
+                    end
+                    error(end+1) = max_error;
                    
 
                 end
